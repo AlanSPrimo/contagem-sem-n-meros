509160687c05e0c0e6c71cc30c2fb947972a81b0
AlanSPrimo <166840180+AlanSPrimo@users.noreply.github.com>
2024-05-13 23:47:30 -0300

    Add files via upload

---
+++ contagem.py
@@ -0,0 +1,10 @@
+#Montar um sistema de contagem, que não utilize nenhum numero no código.
+
+numero = int(input("Digite até qual número deseja contar"))
+listaA = ["a"]
+subtrair = len(listaA)
+
+for i in range(numero):
+              print(numero)
+              numero -= subtrair
+              
